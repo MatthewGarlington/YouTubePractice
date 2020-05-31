cfc947aaddd4392ab1e1b56233e451aa20ce680b
Matthew Garlington <sbmatthewg@gmail.com>
2020-05-31 07:58:32 -0400

    Youtube

--- YoutubeVideoPractice/ViewController.swift
+++ YoutubeVideoPractice/ViewController.swift
@@ -5,16 +5,26 @@
 //  Created by Matthew Garlington on 5/31/20.
 //  Copyright © 2020 Matthew Garlington. All rights reserved.
 //
-
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://youtu.be/Mr8kqQ7Zz38")
+        
+        let req = URLRequest(url: url!)
+        
+        webView.load(req)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
 
 
 }
-
